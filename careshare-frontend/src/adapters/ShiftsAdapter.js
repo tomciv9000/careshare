d84c94601f5284da3b7b1ffb9753971c082843d4
tomciv9000 <eugene.white@gmail.com>
2020-03-22 15:21:22 -0400

    Build Shifts Class and Adapter

---
+++ careshare-frontend/src/adapters/ShiftsAdapter.js
@@ -0,0 +1,19 @@
+class ShiftsAdapter {
+    constructor() {
+      this.baseURL = "http://localhost:3000/shifts"
+    }
+  
+    getShifts() {
+      return fetch(this.baseURL).then(response => response.json()).then(json => json.data)
+    }
+  
+    postShiftToApi(configurationObject) {
+      return fetch(this.baseURL, configurationObject)
+        .then(response => response.json())
+        .catch(error => console.log("Error: " + error))
+    }
+  
+    getPreviousShift(shift) {
+      return fetch(this.baseURL + `/${shift.id}`).then(response => response.json())
+    }
+  }
