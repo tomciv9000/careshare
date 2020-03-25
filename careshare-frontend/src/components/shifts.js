353687260a96ecb93dcb05f56437d9db0062a9b9
tomciv9000 <eugene.white@gmail.com>
2020-03-25 11:07:33 -0400

    Bind event listeners function

--- careshare-frontend/src/components/shifts.js
+++ careshare-frontend/src/components/shifts.js
@@ -2,16 +2,71 @@ class Shifts {
     constructor() {
         this.shifts = []
         this.adapter = new ShiftsAdapter();
-        this.addShiftForm = document.querySelector('#add-shift-form')
-        //this.actionPanel = document.querySelector(".action-container")
-        //this.timelineDiv = document.querySelector('#timeline')
-        this.dropDownDiv = document.querySelector('#all-shifts-dropdown')
-        this.shiftsDropDown = document.getElementById("shifts-dropdown")
-        this.newShiftButton = document.getElementById('new-shift')
-        this.previousShiftsButton = document.getElementById('select-shift')
-        this.formButtons = document.querySelector('.form-show-buttons')
-        this.goBackButton = document.getElementById('go-back')
-        this.actionWrapper = document.getElementById('action-wrapper')
+        this.addShiftForm = document.querySelector('#add-shift-form');
+        this.actionPanel = document.querySelector(".action-container");
+        this.timelineDiv = document.querySelector('#timeline');
+        this.dropDownDiv = document.querySelector('#all-shifts-dropdown');
+        this.shiftsDropDown = document.getElementById("shifts-dropdown");
+        this.newShiftButton = document.getElementById('new-shift');
+        this.previousShiftsButton = document.getElementById('select-shift');
+        this.formButtons = document.querySelector('.form-show-buttons');
+        this.goBackButton = document.getElementById('go-back');
+        this.actionWrapper = document.getElementById('action-wrapper');
+        this.bindEventListeners();
+        this.fetchAndPopulateDropDown();
+    }
 
-        }
+    fetchAndPopulateDropDown() {
+        getShifts().then(json => populateShiftsDropDown(json))
+    }
+
+    bindEventListeners(){
+        this.addShiftForm.addEventListener('submit', e => {
+            e.preventDefault()
+            this.toggleForm()
+            this.toggleGoBack()
+            this.toggleActionPanel()
+            getDates()
+            addNewShift(e)
+            addShiftForm.reset()
+        })
+    }
+
+
+
+
+    addShiftForm.addEventListener('submit', e => {
+        e.preventDefault()
+        toggleForm()
+        toggleGoBack()
+        toggleActionPanel()
+        getDates()
+        addNewShift(e)
+        addShiftForm.reset()
+    })
+    
+    newShiftButton.addEventListener("click", function() {
+        toggleForm();
+        toggleButtons();
+        toggleGoBack();
+    }.bind(this))
+    
+    previousShiftsButton.addEventListener("click", function() {
+        toggleDropDown();
+        toggleButtons();
+        toggleGoBack();
+    }.bind(this))
+    
+    goBackButton.addEventListener("click", function() {
+        goBack()
+    }.bind(this))
+    
+    function bindEventListeners() {
+        shiftsDropDown.addEventListener("change", event => getAndLoadShift(event))
+    }
+    
+
+
+
+
 }
