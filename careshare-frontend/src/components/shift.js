9e37cd25829e7455cc767fa49f14dbd8e94f9104
tomciv9000 <eugene.white@gmail.com>
2020-03-28 03:10:38 -0400

    Refactor binding event listeners and setting colors

--- careshare-frontend/src/components/shift.js
+++ careshare-frontend/src/components/shift.js
@@ -11,8 +11,18 @@ class Shift {
         this.breakfastIcon = document.getElementById('breakfast-icon')
         this.lunchIcon = document.getElementById('lunch-icon')
         this.dinnerIcon = document.getElementById('dinner-icon')
-    
+        this.toggleColorPairs = {
+            "#ffff0091": 'wet-diaper-icon',
+            "#711e1e66": 'soiled-diaper-icon',
+            "#f55cd1cf": 'nap-icon',
+            "#5c87f5cf": 'bedtime-icon',
+            "#00a6ffc8": 'snack-icon',
+            "#ff9900fa": 'breakfast-icon',
+            "#00ff91db": 'lunch-icon',
+            "#6200ffb8": 'dinner-icon'
+        }
     }
+
 
     createShiftTimeline(shift){
         let shiftHeader = document.createElement('h4');
@@ -43,31 +53,31 @@ class Shift {
         return !!option
     }
 
-    bindEventListeners(){
-        this.wetIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.wetIcon, "#ffff0091")
-        }.bind(this));
-        this.soiledIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.soiledIcon, "#711e1e66")
-        }.bind(this));
-        this.snackIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.snackIcon, "#00a6ffc8")
-        }.bind(this));
-        this.breakfastIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.breakfastIcon, "#ff9900fa")
-        }.bind(this));
-        this.lunchIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.lunchIcon, "#00ff91db")
-        }.bind(this));
-        this.dinnerIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.dinnerIcon, "#6200ffb8")
-        }.bind(this));
-        this.napIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.napIcon, "#f55cd1cf")
-        }.bind(this));
-        this.bedtimeIcon.addEventListener("click", function() {
-            this.iconColorToggle(this.bedtimeIcon, "#5c87f5cf")
-        }.bind(this));
+    //bindEventListeners(){
+    //    this.wetIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.wetIcon, "#ffff0091")
+    //    }.bind(this));
+    //    this.soiledIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.soiledIcon, "#711e1e66")
+    //    }.bind(this));
+    //    this.snackIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.snackIcon, "#00a6ffc8")
+    //    }.bind(this));
+    //    this.breakfastIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.breakfastIcon, "#ff9900fa")
+    //    }.bind(this));
+    //    this.lunchIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.lunchIcon, "#00ff91db")
+    //    }.bind(this));
+    //    this.dinnerIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.dinnerIcon, "#6200ffb8")
+    //    }.bind(this));
+    //    this.napIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.napIcon, "#f55cd1cf")
+    //    }.bind(this));
+    //    this.bedtimeIcon.addEventListener("click", function() {
+    //        this.iconColorToggle(this.bedtimeIcon, "#5c87f5cf")
+    //    }.bind(this));
         //this.diaperDone.addEventListener("change", function(event) {
         //    this.getAndLoadShift(event);
         //}.bind(this));
@@ -80,7 +90,8 @@ class Shift {
         //this.noteDone.addEventListener("change", function(event) {
         //    this.getAndLoadShift(event);
         //}.bind(this));
-    }
+    //}
+
 
     wetDiaperSelect(){
         this.iconColorToggle(this.wetIcon, "#ffff0091")
@@ -129,5 +140,16 @@ class Shift {
             icon.style.background = color
         }
     }
+
+    refactoredIconColorToggleandBind(){
+        for (const color in this.toggleColorPairs){ 
+            let el = document.getElementById(`${this.toggleColorPairs[color]}`);
+            el.addEventListener("click", function() {
+                this.iconColorToggle(el, `${color}`)
+            }.bind(this));
+            
+        }
+    }
+
 
   }
