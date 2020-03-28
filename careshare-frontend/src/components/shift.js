238b197d5ad7e2b2a1e128d6ce2577707ebeb2dc
tomciv9000 <eugene.white@gmail.com>
2020-03-28 17:43:41 -0400

    Build function to allow for clicking outside to clear

--- careshare-frontend/src/components/shift.js
+++ careshare-frontend/src/components/shift.js
@@ -11,31 +11,35 @@ class Shift {
         this.breakfastIcon = document.getElementById('breakfast-icon')
         this.lunchIcon = document.getElementById('lunch-icon')
         this.dinnerIcon = document.getElementById('dinner-icon')
-        this.toggleColorPairs = {
-           // "#ffff0091": 'wet-diaper-icon',
-           // "#711e1e66": 'soiled-diaper-icon',
-           // "#f55cd1cf": 'nap-icon',
-            //"#5c87f5cf": 'bedtime-icon',
+        this.toggleSleepPairs = {
+            "#f55cd1cf": 'nap-icon',
+            "#5c87f5cf": 'bedtime-icon'
+        }
+        this.toggleFoodPairs = {
             "#00a6ffc8": 'snack-icon',
             "#ff9900fa": 'breakfast-icon',
             "#00ff91db": 'lunch-icon',
             "#6200ffb8": 'dinner-icon'
         }
+        this.diaperToggleColorPairs = {
+            "#ffff0091": 'wet-diaper-icon',
+            "#711e1e66": 'soiled-diaper-icon'
+        }
+        this.buildDiaperIconEvents()
+        this.buildFoodIconEvents()
+        this.buildSleepIconEvents()
     }
 
 
     createShiftTimeline(shift){
         let shiftHeader = document.createElement('h4');
-        //let subHeader = document.createElement('h5')
         let timelineDiv = document.querySelector('#timeline');
         let closeBtn = document.createElement('button');
         closeBtn.setAttribute('id', 'closeButton')
         let deleteBtn = document.createElement('button');
         deleteBtn.setAttribute('id', 'deleteButton')
         shiftHeader.innerHTML = `Caregiver: ${shift.caregiver}` + "<br />" + `Date: ${DateDisplay.formatDate(shift.date)}`
-        //subHeader.innerText = `Caregiver: ${shift.caregiver}`
         timelineDiv.append(shiftHeader);
-        //timelineDiv.append(subHeader);
         if (this.shiftFound(shift.id)) {
             closeBtn.innerText = 'Review Complete'
             deleteBtn.innerText = 'Delete Shift'
@@ -52,15 +56,9 @@ class Shift {
         let option = shiftsDropDown.querySelector('[value="' + id + '"]');
         return !!option
     }
-
-
     
     iconSelected(icon){
         return !icon.style.background == ""
-    }
-
-    iconReset(icon){
-        icon.style.background = ""
     }
 
     iconColorToggle(icon, color){
@@ -69,35 +67,60 @@ class Shift {
         icon.children[1].style.filter="invert(100%)"
     }
 
-    buildIconEvents(){
-        const icons = [...document.getElementsByClassName('icons')].slice(0,4)
-        for (const color in this.toggleColorPairs){ 
-            let el = document.getElementById(`${this.toggleColorPairs[color]}`);
-            el.addEventListener("click", function() {
-                this.resetIcons(icons)
-                this.iconColorToggle(el, `${color}`)
-            }.bind(this));
-            
+    iconMultiSelect(icon, color){
+        if (this.iconSelected(icon)) {
+            this.resetIcons(icon)
+        } else {
+            this.iconColorToggle(icon, color)
         }
     }
 
-    foodValueChanged() {
-        if (document.getElementById("snack").checked == true) {
-           this.snackIcon.background = "#00a6ffc8"
-        } else if (document.getElementById("breakfast").checked == true) {
-            this.breakfastIcon.background = "#00a6ffc8"
-        } else if (document.getElementById("lunch").checked == true) {
-            this.lunchIcon.background = "#00a6ffc8"
-        } else if (document.getElementById("dinner").checked == true) {
-            this.dinnerIcon.background = "#00a6ffc8"
+    buildDiaperIconEvents(){
+        for (let color in this.diaperToggleColorPairs){ 
+            let el = document.getElementById(`${this.diaperToggleColorPairs[color]}`);
+            el.addEventListener("click", function() {
+                this.iconMultiSelect(el, `${color}`)
+            }.bind(this));
         }
     }
-    
-    //works
-    resetIcons(iconArray) {
-        for (let index = 0; index < iconArray.length; index++) {
-            const element = iconArray[index];
-            if (!element.style.background == "") {
+
+    buildSleepIconEvents(){
+        const sleepIcons = [...document.getElementsByClassName('sleep-radio-icons')]
+        for (let color in this.toggleSleepPairs){ 
+            let el = document.getElementById(`${this.toggleSleepPairs[color]}`);
+            el.addEventListener("click", function() {
+                this.resetIcons(sleepIcons)
+                this.iconMultiSelect(el, `${color}`)
+            }.bind(this));
+        }
+    }
+
+
+    buildFoodIconEvents(){
+        const foodIcons = [...document.getElementsByClassName('food-radio-icons')]
+        for (let color in this.toggleFoodPairs){ 
+            let el = document.getElementById(`${this.toggleFoodPairs[color]}`);
+            el.addEventListener("click", function() {
+                this.resetIcons(foodIcons)
+                this.iconMultiSelect(el, `${color}`)
+            }.bind(this));
+        }
+        //const sleepIcons = [...document.getElementsByClassName('sleep-radio-icons')]
+        //for (let color in this.toggleColorPairs){ 
+        //    let sleepEl = document.getElementById(`${this.toggleColorPairs[color]}`);
+        //    sleepEl.addEventListener("click", function() {
+        //        this.resetIcons(sleepIcons)
+        //        this.iconMultiSelect(sleepEl, `${color}`)
+        //    }.bind(this));
+        //}
+        
+    }
+
+    resetIcons(icons) {
+        let toBeToggled = [].concat(icons || [])
+        for (let index = 0; index < toBeToggled.length; index++) {
+            const element = toBeToggled[index];
+            if (this.iconSelected(element)){
                 element.style.background = ""
                 element.lastElementChild.style.color = "#656167"
                 element.children[1].style.filter=""
@@ -105,5 +128,26 @@ class Shift {
         }
     }
 
+    clickingOutsideClears(){
+        document.addEventListener("click", (evt) => {
+            const flyoutElement = document.getElementById("farts");
+            let targetElement = evt.target; // clicked element
+        
+            do {
+                if (targetElement == flyoutElement) {
+                    // This is a click inside. Do nothing, just return.
+                    console.log('you clicked inside')
+                    return
+                }
+                // Go up the DOM
+                targetElement = targetElement.parentNode;
+            } while (targetElement);
+        
+            // This is a click outside.
+            let fartTest = [...document.getElementsByClassName('food-radio-icons')];
+            this.resetIcons(fartTest)
+        });
+    }
 
-  }
+
+}
